d2653ca736d2e4fa2f505d1bc53b9ab1202b2a18
Burak Yıldız <cang34gs@gmail.com>
2022-07-09 10:58:51 +0300

    HardwareControl
    Shows computer hardware in detail

---
+++ sistemozellikleri1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace sistemozellikleri1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.Gpuinfo = new MetroFramework.Controls.MetroLabel();
+            this.Raminfo = new MetroFramework.Controls.MetroLabel();
+            this.OperatingSystem = new MetroFramework.Controls.MetroLabel();
+            this.BuildTime = new MetroFramework.Controls.MetroLabel();
+            this.CoreNumber = new MetroFramework.Controls.MetroLabel();
+            this.Cpuinfo = new MetroFramework.Controls.MetroLabel();
+            this.core = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel16 = new MetroFramework.Controls.MetroLabel();
+            this.Harddisk = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(281, 509);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(142, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "program çalışma tarihi";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(38, 254);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(35, 19);
+            this.metroLabel4.TabIndex = 3;
+            this.metroLabel4.Text = "CPU";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(39, 326);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(89, 19);
+            this.metroLabel5.TabIndex = 4;
+            this.metroLabel5.Text = "işletim sistemi";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(38, 224);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(36, 19);
+            this.metroLabel6.TabIndex = 5;
+            this.metroLabel6.Text = "Ram";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(39, 107);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(72, 19);
+            this.metroLabel7.TabIndex = 6;
+            this.metroLabel7.Text = "Ekran Kartı";
+            // 
+            // Gpuinfo
+            // 
+            this.Gpuinfo.AutoSize = true;
+            this.Gpuinfo.ForeColor = System.Drawing.Color.Black;
+            this.Gpuinfo.Location = new System.Drawing.Point(281, 107);
+            this.Gpuinfo.Name = "Gpuinfo";
+            this.Gpuinfo.Size = new System.Drawing.Size(55, 19);
+            this.Gpuinfo.TabIndex = 13;
+            this.Gpuinfo.Text = "Gpuinfo";
+            // 
+            // Raminfo
+            // 
+            this.Raminfo.AutoSize = true;
+            this.Raminfo.Location = new System.Drawing.Point(281, 224);
+            this.Raminfo.Name = "Raminfo";
+            this.Raminfo.Size = new System.Drawing.Size(58, 19);
+            this.Raminfo.TabIndex = 12;
+            this.Raminfo.Text = "Raminfo";
+            // 
+            // OperatingSystem
+            // 
+            this.OperatingSystem.AutoSize = true;
+            this.OperatingSystem.Location = new System.Drawing.Point(281, 326);
+            this.OperatingSystem.Name = "OperatingSystem";
+            this.OperatingSystem.Size = new System.Drawing.Size(110, 19);
+            this.OperatingSystem.TabIndex = 11;
+            this.OperatingSystem.Text = "OperatingSystem";
+            // 
+            // BuildTime
+            // 
+            this.BuildTime.AutoSize = true;
+            this.BuildTime.Location = new System.Drawing.Point(523, 509);
+            this.BuildTime.Name = "BuildTime";
+            this.BuildTime.Size = new System.Drawing.Size(67, 19);
+            this.BuildTime.TabIndex = 9;
+            this.BuildTime.Text = "BuildTime";
+            // 
+            // CoreNumber
+            // 
+            this.CoreNumber.AutoSize = true;
+            this.CoreNumber.Location = new System.Drawing.Point(281, 288);
+            this.CoreNumber.Name = "CoreNumber";
+            this.CoreNumber.Size = new System.Drawing.Size(87, 19);
+            this.CoreNumber.TabIndex = 16;
+            this.CoreNumber.Text = "CoreNumber";
+            // 
+            // Cpuinfo
+            // 
+            this.Cpuinfo.AutoSize = true;
+            this.Cpuinfo.Location = new System.Drawing.Point(281, 254);
+            this.Cpuinfo.Name = "Cpuinfo";
+            this.Cpuinfo.Size = new System.Drawing.Size(55, 19);
+            this.Cpuinfo.TabIndex = 17;
+            this.Cpuinfo.Text = "Cpuinfo";
+            // 
+            // core
+            // 
+            this.core.AutoSize = true;
+            this.core.Location = new System.Drawing.Point(373, 288);
+            this.core.Name = "core";
+            this.core.Size = new System.Drawing.Size(35, 19);
+            this.core.TabIndex = 18;
+            this.core.Text = "core";
+            // 
+            // metroLabel16
+            // 
+            this.metroLabel16.AutoSize = true;
+            this.metroLabel16.Location = new System.Drawing.Point(38, 380);
+            this.metroLabel16.Name = "metroLabel16";
+            this.metroLabel16.Size = new System.Drawing.Size(34, 19);
+            this.metroLabel16.TabIndex = 20;
+            this.metroLabel16.Text = "Hdd";
+            // 
+            // Harddisk
+            // 
+            this.Harddisk.AutoSize = true;
+            this.Harddisk.Location = new System.Drawing.Point(278, 380);
+            this.Harddisk.Name = "Harddisk";
+            this.Harddisk.Size = new System.Drawing.Size(60, 19);
+            this.Harddisk.TabIndex = 21;
+            this.Harddisk.Text = "Harddisk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 622);
+            this.Controls.Add(this.Harddisk);
+            this.Controls.Add(this.metroLabel16);
+            this.Controls.Add(this.core);
+            this.Controls.Add(this.Cpuinfo);
+            this.Controls.Add(this.CoreNumber);
+            this.Controls.Add(this.Gpuinfo);
+            this.Controls.Add(this.Raminfo);
+            this.Controls.Add(this.OperatingSystem);
+            this.Controls.Add(this.BuildTime);
+            this.Controls.Add(this.metroLabel7);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Name = "Form1";
+            this.Text = "Sistem özellikleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroLabel Gpuinfo;
+        private MetroFramework.Controls.MetroLabel Raminfo;
+        private MetroFramework.Controls.MetroLabel OperatingSystem;
+        private MetroFramework.Controls.MetroLabel BuildTime;
+        private MetroFramework.Controls.MetroLabel CoreNumber;
+        private MetroFramework.Controls.MetroLabel Cpuinfo;
+        private MetroFramework.Controls.MetroLabel core;
+        private MetroFramework.Controls.MetroLabel metroLabel16;
+        private MetroFramework.Controls.MetroLabel Harddisk;
+    }
+}
+
---
+++ sistemozellikleri1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Deployment.Application;
+using System.Collections;
+using System.Management;
+
+namespace sistemozellikleri1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CPU();
+            HDD();
+            BuildTime.Text = BUILD_DATE();
+            OperatingSystem.Text = OS();
+            Raminfo.Text = RAM();
+            Gpuinfo.Text = EKRANKART();
+        }
+
+        public class HardDrive
+        {
+            public string Model { get; set; }
+            public string InterfaceType { get; set; }
+            public string Caption { get; set; }
+            public string SerialNo { get; set; }
+        }
+        private void HDD()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            long totalSize = 0;
+            foreach (ManagementObject disk in searcher.Get())
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    if (drive.IsReady)// şeklinde IsReady olanları almalısın.
+                        totalSize += drive.TotalSize / 1024 / 1024 / 1024;
+                }
+                Harddisk.Text = "Model: " + disk["Model"] + "\n InterfaceType: " + disk["InterfaceType"] + "\nBoyut: " + disk["Size"] + "\npart:" + disk["Partitions"] +"\nboyut" + totalSize.ToString() + " Gb";
+            }
+
+        }
+
+
+        private string BUILD_DATE()
+        {
+            DateTime buildDate = new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+            return buildDate.ToString();
+        }
+
+        public void CPU()
+        {
+            CoreNumber.Text = System.Environment.ProcessorCount.ToString();
+            RegistryKey Rkey1 = Registry.LocalMachine;
+            Rkey1 = Rkey1.OpenSubKey("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0");
+            Cpuinfo.Text = (string)Rkey1.GetValue("ProcessorNameString").ToString();
+        }
+
+        private string OS()//İşletim Sistemi Özellikleri İçin
+        {
+            return System.Environment.OSVersion.ToString();
+        }
+
+        public static string RAM()
+        {
+            string ramSizeInfo = null;
+            ManagementObjectSearcher ramSearcher = new ManagementObjectSearcher("Select * From Win32_ComputerSystem");
+
+            foreach (ManagementObject mObject in ramSearcher.Get())
+            {
+                double Ram_Bytes = (Convert.ToDouble(mObject["TotalPhysicalMemory"]));
+                double ramgb = Ram_Bytes / 1073741824;
+                double ramSize = Math.Ceiling(ramgb);
+                ramSizeInfo = ramSize.ToString() + " GB";
+            }
+            return ramSizeInfo;
+        }
+
+        public static string EKRANKART()
+        {
+            string videoControllerInfo = null;
+            string name = null;
+            string ram = null;
+            string horizontalResolution = null;
+            string verticalResolution = null;
+            string deviceID = null;
+
+            ManagementObjectSearcher vidSearcher = new ManagementObjectSearcher("Select * from Win32_VideoController Where availability='3'");
+
+            foreach (ManagementObject mObject in vidSearcher.Get())
+            {
+                name = mObject["name"].ToString();
+                ram = (Convert.ToDouble(mObject["AdapterRam"]) / 1073741824).ToString();
+                deviceID = (string)mObject["DeviceID"];
+                horizontalResolution = mObject["CurrentHorizontalResolution"].ToString();
+                verticalResolution = mObject["CurrentVerticalResolution"].ToString();
+            }
+            videoControllerInfo = name + "\r\n Ram Miktarı : " + ram + " GB \r\n ID : " + deviceID + "\r\n Çözünürlük :" + horizontalResolution + " x " + verticalResolution;
+
+            return videoControllerInfo;
+        }
+
+        private void lblMac_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
